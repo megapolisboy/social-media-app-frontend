9ea13764f6b63028368e8073ddbfdc383428070c
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-07-23 00:20:50 +0300

    backend origin changed

--- src/api/stories.ts
+++ src/api/stories.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://social-media-app-introvert.herokuapp.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://social-media-app-introvert.herokuapp.com",
+  // baseURL: "http://localhost:5000",
 });
 
 api.interceptors.request.use((req) => {
